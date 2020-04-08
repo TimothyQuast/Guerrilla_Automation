75611803ffa90c1621a28f363e9d1224b31e7a73
Timothy Quast <timothy.m.quast@gmail.com>
2020-04-08 15:06:33 -0500

    changed filenames and wrote more words in article

--- Guerrilla Automation.R
+++ Guerrilla Automation.R
@@ -10,6 +10,14 @@ main = function(){
   library(writexl)
   library(dplyr)
   
+  #Run each step in the process successively.
+  #Alternatively, to accomplish the same thing, simply run the following (which runs back through each step):
+  #output_results()
+  
+  input_control = import_input_control()
+  input_list = gather_input_data(input_control)
+  aggregate_list = aggregate_input_data(input_list)
+  output_results(aggregate_list)
   
   
 }
@@ -17,7 +25,7 @@ main = function(){
 
 #Returns Input Control from Process Spreadsheet
 # filepath - the file path to the Process Spreadsheet
-import_input_control = function(filepath = "Excel Files/v1/Process Spreadsheet.xlsx"){
+import_input_control = function(filepath = "Excel Files/Process Spreadsheet.xlsx"){
 
   
   #9001 is chosen arbitrarily, we just need a large number and 9001 is OVER 9000!!!
@@ -30,16 +38,19 @@ import_input_control = function(filepath = "Excel Files/v1/Process Spreadsheet.x
 
 }
 
-#Returns input data from input excel files
+#Returns a list with two data frames: input_control - tracks the excel input files, input_data - contains the granular input data
 # input_control - dataframe which tracks inputs with columns "Filepath", "Sheet", and "Range"
 gather_input_data = function(input_control = import_input_control()){
   
+  #Using a list to store input data because lists can store multiple data frames
+  #One data frame will show which Excel files imported successfully (+if not: why not?) and the other will have the input data.
   input_list = list()
   
+  #initialize data frame to stor input data
   input_data = data.frame(matrix(nrow = 0, ncol = 5))
   names(input_data) = c("Filepath", "Sheet", "Range","Account Number", "Amount")
   
-  #File_Is_Good and Error_Message can be used to track which files came in successfully and if not, what went wrong
+  #File_Is_Good and Error_Message are used to track which files came in successfully and if not, what went wrong
   File_Is_Good = logical()
   Error_Message = character()
   
@@ -65,18 +76,16 @@ gather_input_data = function(input_control = import_input_control()){
       #append input_temp to input_data
       input_data = rbind(input_data, input_temp)
       
-      #Assign File_Is_Good = TRUE and Error_Message ="" assuming everything went well
-      return(c(TRUE,""))
-      
+      #Import successful
+      Error_Handle = c(TRUE,"")
       
     },
     error = function(err){
-      #Assign File_Is_Good=FALSE and Error_Message=err when there is an error
-      return(FALSE,as.character(err))
+      #Import unsuccessful
+      return(c(FALSE, as.character(err)))
     })
     
-    print(class(input_data))
-    
+    #Assign new import control columns to track which imports were successful
     File_Is_Good[i] = as.logical(Error_Handle[1])
     Error_Message[i] = Error_Handle[2]
     
@@ -86,32 +95,39 @@ gather_input_data = function(input_control = import_input_control()){
   #Add File_Is_Good and Error_Message columns to the input_control dataframe
   input_control = cbind(input_control, File_Is_Good, Error_Message)
   
+  #Assign the appropriate data frames to the input_list
   input_list[[1]] = input_control
   input_list[[2]] = input_data
   
   return(input_list)
 }
 
-#
-# input_data - dataframe with input data from excel input files
+# Summarizes input data and returns a list of relevant data frames
+# input_list[[1]] - dataframe tracking input from excel files
+# input_list[[2]] - dataframe with input data from excel input files
 aggregate_input_data = function(input_list = gather_input_data()){
   
   #initialize list to store output data - a list will be helpful for interacting with writexl
   aggregate_list = list()
   
   #first list item: a report on which input files came through properly
-  aggregate_list[[1]] = input_list[[1]]
+  aggregate_list[["input_control"]] = input_list[[1]]
   
   #second list item: the input data itself
-  aggregate_list[[2]] = input_list[[2]]
+  aggregate_list[["input_data"]] = input_list[[2]]
   
   #third list item: a summary of the data; I use the British spelling of 'summarise' because it makes me feel more intelligent (though either spelling works).
-  aggregate_list[[3]] = data.frame(input_data %>% group_by(`Account Number`) %>% summarise(Amount = sum(Amount)))
+  aggregate_list[["summary"]] = data.frame(input_list[[2]] %>% group_by(`Account Number`) %>% summarise(Amount = sum(Amount)))
   
   return(aggregate_list)
 }
 
-output_results = function(){}
+#Output the results into a single excel file
+output_results = function(aggregate_list = aggregate_input_data()){
+  
+  write_xlsx(aggregate_list, "./Excel Files/R Outputs/Results.xlsx")
+  
+}
 
 
 
